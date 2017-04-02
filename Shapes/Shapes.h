d2acd7a46236568562849081e7321abd95ab97bf
christian otkjær <christian.otkjaer@gmail.com>
2017-04-02 14:59:37 +0200

    Initial Commit

---
+++ Shapes/Shapes.h
@@ -0,0 +1,19 @@
+//
+//  Shapes.h
+//  Shapes
+//
+//  Created by Christian Otkjær on 02/04/17.
+//  Copyright © 2017 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shapes.
+FOUNDATION_EXPORT double ShapesVersionNumber;
+
+//! Project version string for Shapes.
+FOUNDATION_EXPORT const unsigned char ShapesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shapes/PublicHeader.h>
+
+
